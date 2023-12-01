454c8ffa5d2a43ed859d8ba2ddd35626c705c692
maidan1 <136680901+maidan1@users.noreply.github.com>
2023-12-01 16:33:21 +0200

    Update CreateCardPage.jsx

--- src/pages/CreateCardPage/CreateCardPage.jsx
+++ src/pages/CreateCardPage/CreateCardPage.jsx
@@ -1,165 +1,260 @@
-import React, { useEffect, useState } from "react";
-import CardComponent from "../../components/CardComponent";
-import axios from "axios";
-import { useSelector } from "react-redux";
-import { Container, Grid, Button, Typography, Divider } from "@mui/material";
-import Pagination from "@mui/material/Pagination";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
+import {
+  Container,
+  TextField,
+  Grid,
+  Typography,
+  Divider,
+  Button,
+} from "@mui/material";
+import { Link, useNavigate } from "react-router-dom"; // Import useNavigate
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import ROUTES from "../../routes/ROUTES";
+import axios from "axios";
 
-const MyCardsPage = () => {
-  const [myCards, setMyCards] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const location = useLocation();
-  const userData = useSelector((state) => state.authSlice.userData);
-  const navigate = useNavigate();
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 8;
+const CreateCardPage = () => {
+  const navigate = useNavigate(); // Use useNavigate to get the navigate function
+  const [inputsValue, setInputValue] = useState({
+    title: "",
+    subtitle: "",
+    phone: "",
+    add: "",
+    mail: "",
+    description: "",
+    web: "",
+    url: "",
+    alt: "",
+    state: "",
+    country: "",
+    city: "",
+    street: "",
+    houseNumber: "",
+    zip: "",
+  });
 
-  useEffect(() => {
-    const fetchMyCards = async () => {
-      try {
-        setLoading(true);
-
-        if (!userData) {
-          console.error("User data is undefined");
-          setLoading(false);
-          return;
-        }
-
-        const userId = userData._id;
-        const config = {
-          headers: {
-            "x-auth-token": process.env.REACT_APP_API_TOKEN,
-          },
-        };
-
-        const response = await axios.get(
-          `https://monkfish-app-z9uza.ondigitalocean.app/bcard2/cards/my-cards`,
-          config
-        );
-
-        const userCards = response.data.filter(
-          (card) => card.user_id === userId
-        );
-
-        const cardsWithLikeStatus = userCards.map((card) => ({
-          ...card,
-          like: localStorage.getItem(`like_${card._id}`) === "true",
-        }));
-
-        setMyCards(cardsWithLikeStatus);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching user's cards:", error);
-        setError("Error fetching user's cards. Please try again.");
-        setLoading(false);
-      }
-    };
-
-    fetchMyCards();
-  }, [userData, location.pathname]);
-
-  const handleEditCard = (_id) => {
-    navigate(`${ROUTES.EDITCARD}/${_id}`);
+  const handleInputChange = (e) => {
+    setInputValue((currentState) => ({
+      ...currentState,
+      [e.target.id]: e.target.value,
+    }));
   };
 
-  const handleDeleteCard = async (_id) => {
+  const handleUpdateChangesClick = async () => {
     try {
-      const config = {
-        headers: {
-          "x-auth-token": process.env.REACT_APP_API_TOKEN,
+      const { data } = await axios.post("/cards", {
+        title: inputsValue.title,
+        subtitle: inputsValue.subtitle,
+        description: inputsValue.description,
+        phone: inputsValue.phone,
+        email: inputsValue.mail,
+        web: inputsValue.web,
+        image: {
+          url: inputsValue.url,
+          alt: inputsValue.alt,
         },
-      };
+        address: {
+          state: inputsValue.state,
+          country: inputsValue.country,
+          city: inputsValue.city,
+          street: inputsValue.street,
+          houseNumber: inputsValue.houseNumber,
+          zip: +inputsValue.zip,
+        },
+      });
 
-      await axios.delete(
-        `https://monkfish-app-z9uza.ondigitalocean.app/bcard2/cards/${_id}`,
-        config
-      );
+      toast.success("You've created a business card!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
 
-      setMyCards((prevCards) => prevCards.filter((card) => card._id !== _id));
-    } catch (error) {
-      console.error("Error deleting card:", error);
-      setError("Error deleting card. Please try again.");
+      navigate(ROUTES.MYCARDSPAGE);
+    } catch (err) {
+      console.error("Error creating card:", err);
+
+      toast.error("Error creating card. Please try again.", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     }
   };
 
-  const handleLikeChange = (_id, newLikeStatus) => {
-    setMyCards((prevCards) =>
-      prevCards.map((card) =>
-        card._id === _id ? { ...card, like: newLikeStatus } : card
-      )
-    );
-
-    // Save the like status in local storage
-    localStorage.setItem(`like_${_id}`, newLikeStatus.toString());
-  };
-
-  const handlePageChange = (event, page) => {
-    setCurrentPage(page);
-  };
-
   return (
-    <Container
-      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
-      <Typography
-        sx={{ fontFamily: "serif", textAlign: "center", p: 5 }}
-        variant="h1"
-      >
-        MY CARDS
-        <Divider sx={{ mt: 4, width: 750 }} />
+    <Container sx={{ padding: "50px" }}>
+      <Typography variant="h2" sx={{ mb: 1, padding: "10px", pb: "0px" }}>
+        Create - Cards
       </Typography>
-      <Divider sx={{ mb: 2 }} />
-      <Button
-        variant="outlined"
-        sx={{ mt: 3, bgcolor: "green", color: "white" }}
-        onClick={() => navigate(ROUTES.CREATECARD)}
-      >
-        Create Card
-      </Button>
+      <Typography variant="body1" sx={{ mb: 1, padding: "3px", ml: "7px" }}>
+        Put new values in the correct input
+      </Typography>
+      <Divider sx={{ mb: 3 }} />
+      <Grid container flexDirection={"column"}>
+        <TextField
+          id="title"
+          label="Title"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.title}
+          required
+        />
+        <TextField
+          id="subtitle"
+          label="SubTitle"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.subtitle}
+          required
+        />
+        <TextField
+          id="phone"
+          label="Phone Number"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.phone}
+          required
+        />
+        <TextField
+          id="description"
+          label="Description"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.description}
+          required
+        />
+        <TextField
+          id="web"
+          label="Web"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.web}
+        />
+        <TextField
+          id="mail"
+          label="Email"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.mail}
+          required
+        />
 
-      {loading && <div>Loading...</div>}
-      {error && <div>{error}</div>}
+        <TextField
+          id="url"
+          label="Url"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.url}
+        />
+        <TextField
+          id="alt"
+          label="Alt"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.alt}
+        />
 
-      <Grid container spacing={2} sx={{ marginTop: 2 }}>
-        {myCards
-          .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
-          .map(({ _id, title, subtitle, phone, address, image, like }) => (
-            <Grid item key={_id} xs={12} sm={6} md={4} lg={3}>
-              <CardComponent
-                _id={_id}
-                title={title}
-                subTitle={subtitle}
-                phone={phone}
-                address={`${address.city}, ${address.street} ${address.houseNumber}`}
-                img={image && image.url}
-                alt={image && image.alt}
-                like={like}
-                onDeleteCard={handleDeleteCard}
-                onEditCard={handleEditCard}
-                onLikeChange={handleLikeChange}
-              />
-            </Grid>
-          ))}
+        <TextField
+          id="state"
+          label="State"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.state}
+        />
+        <TextField
+          id="country"
+          label="Country"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.country}
+          required
+        />
+        <TextField
+          id="city"
+          label="City"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.city}
+          required
+        />
+        <TextField
+          id="street"
+          label="Street"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.street}
+          required
+        />
+        <TextField
+          id="houseNumber"
+          label="House Number"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.houseNumber}
+          required
+        />
+        <TextField
+          id="zip"
+          label="Zip"
+          variant="outlined"
+          sx={{ mt: "10px" }}
+          onChange={handleInputChange}
+          value={inputsValue.zip}
+        />
       </Grid>
-
-      <Pagination
-        count={Math.ceil(myCards.length / itemsPerPage)}
-        page={currentPage}
-        onChange={handlePageChange}
-        color="primary"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "flex-end",
-          marginTop: "20px",
-          marginBottom: "20px",
-        }}
-      />
+      <Grid container spacing={2}>
+        <Grid item lg={8} md={8} sm={8} xs>
+          <Link to="/MyCardsPage" variant="body2">
+            <Button
+              variant="outlined"
+              sx={{ mt: 2, width: "100%", ml: "0%", bgcolor: "darkblue" }}
+              onClick={handleUpdateChangesClick}
+            >
+              Create Card
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs>
+          <Link to={ROUTES.HOME}>
+            <Button
+              variant="outlined"
+              sx={{
+                mt: 2,
+                width: "100%",
+                ml: "0%",
+                bgcolor: "navy",
+                color: "gray",
+              }}
+            >
+              Discard
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
-
-export default MyCardsPage;
+export default CreateCardPage;
